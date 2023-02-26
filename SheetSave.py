f26802d0f42531ab21c216fbdaf1b84babac9980
Eupheon <ottohillegass@gmail.com>
2023-02-26 12:40:38 -0500

    Pit Scouting setups

--- SheetSave.py
+++ SheetSave.py
@@ -3,7 +3,7 @@ import shutil
 from time import sleep
 from datetime import datetime
 from tkinter import *
-from pynput.keyboard import Key, Controller
+from pandas import read_excel
 
 # Find user's home directory
 home = os.path.expanduser("~")
@@ -21,8 +21,14 @@ scouting = f"{desktop}\\Scouting"
 
 def main():
 
-    keyboard = Controller()
-    
+    # Open Save File window
+    root = Tk()
+    r=runningWindow(root)
+    root.title("Saving...")
+    root.attributes("-topmost", True)
+    root.mainloop()
+
+
     # Find Excel 2016
     drives = os.popen("wmic logicaldisk get name").read().split()
     for drive in drives:
@@ -35,70 +41,92 @@ def main():
     else:
         print("Excel 2016 not found!")
         return
-    
-    # Focus Excel 2016
-    keyboard.press(Key.alt)
-    keyboard.press(Key.tab)
-    keyboard.release(Key.tab)
-    keyboard.release(Key.alt)
-    sleep(.5)
-
-    # Save file
-    keyboard.press(Key.ctrl)
-    keyboard.press('s')
-    keyboard.release('s')
-    keyboard.release(Key.ctrl)
-    sleep(1.5)
 
     # Close Excel 2016
-    keyboard.press(Key.alt)
-    keyboard.press(Key.f4)
-    keyboard.release(Key.f4)
-    keyboard.release(Key.alt)
+    os.system("taskkill /im EXCEL.EXE")
 
     # Open Save File window
     root = Tk()
-    m=mainWindow(root)
+    rtp=mainWindow(root)
     root.title("Save File")
     root.attributes("-topmost", True)
     root.mainloop()
-    
-    # Open Excel 2016
-    os.system(f"\"{scouting}\\EDIT_Match_Scouting.xlsm\"")
 
-def filesave(team,match):
+
+def filesave(team):
 
     # Find removable matches drive
     drives = os.popen("wmic logicaldisk get name").read().split()
     for drive in drives:
-        if(os.path.isdir(f"{drive}\\Matches") == True):
-            matches = f"{drive}\\Matches"
+        if(os.path.isdir(f"{drive}\\Pits") == True):
+            matches = f"{drive}\\Pits"
             break
     else:
-        print("Matches drive not found! Please insert the matches drive and try again. If the problem persists, please contact the Scouting Leads.")
+        root = Tk()
+        nf=notFoundWindow(root)
+        root.title("Error")
+        root.attributes("-topmost", True)
+        root.mainloop()
         return
 
-    shutil.copyfile(f"{scouting}\\EDIT_Match_Scouting.xlsm", f"{matches}\\M{match}-T{team}-({datetime.now().strftime('%m-%d-%y')}).xlsm")
-    shutil.copyfile(f"{scouting}\\BLANK_Match_Scouting.xlsm", f"{scouting}\\EDIT_Match_Scouting.xlsm")
+    shutil.copyfile(f"{scouting}\\EDIT_Pit_Scouting.xlsm", f"{matches}\\PIT-T{team}-({datetime.now().strftime('%m-%d-%y')}).xlsm")
+    shutil.copyfile(f"{scouting}\\BLANK_Pit_Scouting.xlsm", f"{scouting}\\EDIT_Pit_Scouting.xlsm")
 
 class mainWindow(object):
     def __init__(self,master):
+
+        # Find Team # and Match # w/ Pandas
+        while True:
+            try:
+                df = read_excel(f"{scouting}\\EDIT_Pit_Scouting.xlsm", header=None)
+                break
+            except:
+                sleep(.5)
+        team = df.iloc[2,1]
+        # Close Pandas
+        del df
+
         self.master=master
         self.lTeam=Label(master,text=("Enter your team number: "))
         self.lTeam.pack()
         self.eTeam=Entry(master)
+        self.eTeam.insert(0,team)
         self.eTeam.pack()
-        self.lMatch=Label(master,text=("Enter your match number: "))
-        self.lMatch.pack()
-        self.eMatch=Entry(master)
-        self.eMatch.pack()
         self.b=Button(master,text="Save",state="normal",command=lambda: self.cleanup())
         self.b.pack()
     
     def cleanup(self):
         team = self.eTeam.get()
-        match = self.eMatch.get()
-        filesave(team,match)
+        filesave(team)
+        self.master.destroy()
+
+class runningWindow(object):
+    def __init__(self,master):
+        self.master=master
+        self.l=Label(master,text="Saving...")
+        self.l.pack()
+        self.lRemembers=Label(master,text="Ensure that the file is saved and that the matches drive is inserted. \nIf the matches drive is not inserted, the file will not be saved.")
+        self.lRemembers.pack()
+        self.b=Button(master,text="Ready",command=lambda: self.cleanup())
+        self.b.pack()
+        self.bCancel=Button(master,text="Cancel",command=lambda: self.cancel())
+        self.bCancel.pack()
+
+    def cleanup(self):
+        self.master.destroy()
+    
+    def cancel(self):
+        os.system("taskkill /f /im python.exe")
+        self.master.destroy()
+
+class notFoundWindow(object):
+    def __init__(self,master):
+        self.master=master
+        self.l=Label(master,text="Error: Matches drive not found! Please insert the matches drive and try again. \nIf the problem persists, please contact the Scouting Leads.")
+        self.l.pack()
+        self.b=Button(master,text="Close",command=lambda: self.cleanup())
+
+    def cleanup(self):
         self.master.destroy()
 
 if __name__ == "__main__":
